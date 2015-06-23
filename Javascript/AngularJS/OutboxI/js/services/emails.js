20ea1130d069096c52ac12e3c9fd9ce07d4fe20f
oscavi <oscavi@gmail.com>
2015-06-23 18:23:58 +0200

    Outbox I
    End

--- Javascript/AngularJS/OutboxI/js/services/emails.js
+++ Javascript/AngularJS/OutboxI/js/services/emails.js
@@ -1,9 +1,12 @@
 app.factory('emails', ['$http', function($http) {
   return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/emails.json')
             .success(function(data) {
+              console.log(data);
               return data;
             })
             .error(function(err) {
+              console.log(err);
               return err;
             });
 }]);
+s
